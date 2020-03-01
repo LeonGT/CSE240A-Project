8cbc7d4845b3e6491673106998ad826509eb6f13
leon <lxa1997@hotmail.com>
2020-02-29 22:04:07 -0800

    Add script for running all experiments

---
+++ src/exp.sh
@@ -0,0 +1,10 @@
+for trace in ../traces/*
+do
+    echo "Running gshare:13 on $trace"
+    bunzip2 -kc "$trace" | ./predictor --gshare:13 | sed '$!d'
+done
+for trace in ../traces/*
+do
+    echo "Running tournament:9:10:10 on $trace"
+    bunzip2 -kc "$trace" | ./predictor --tournament:9:10:10 | sed '$!d'
+done
